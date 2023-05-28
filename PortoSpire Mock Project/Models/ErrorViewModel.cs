f86d00c692b67bf00d3152a8c8474a5ef61cc056
Jake <jmorgan3142001@gmail.com>
2023-05-28 10:41:11 -0500

    Initial Commit

---
+++ PortoSpire Mock Project/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace PortoSpire_Mock_Project.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
